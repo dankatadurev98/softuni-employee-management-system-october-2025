95ce5a37594116ca2c5703c04d808690a982238e
dankatadurev98 <dankatadurev@gmail.com>
2025-11-10 15:24:14 +0200

    Add base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
+import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Pagination from "./components/Pagination"
+import SearchForm from "./components/SearchForm"
+import UserList from "./components/UserList"
+
 
 function App() {
-  
+
 
   return (
-    <header>
+    <main>
       <Header />
-  
-    </header>
+
+      <main className="main">
+        <section className="card users-container">
+
+          <SearchForm />
+          
+          <UserList />
+          
+          <button className="btn-add btn">Add new user</button>
+
+          <Pagination />
+
+        </section>
+
+
+      </main>
+
+
+      <Footer />
+    </main>
   )
 }
 
