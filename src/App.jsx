0aee11cb3990c7ed2f25c3022990aeed1fe082e4
dankatadurev98 <dankatadurev@gmail.com>
2025-11-10 14:09:37 +0200

    Attaching first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Header from "./components/Header"
+
 function App() {
   
 
   return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
+    <header>
+      <Header />
+  
+    </header>
   )
 }
 
